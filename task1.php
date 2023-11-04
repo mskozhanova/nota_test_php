9d3791c63bb7a3c48e935f0536a654cf0803928a
lokesh0571995 <44475730+lokesh0571995@users.noreply.github.com>
2023-11-05 00:05:34 +0530

    Update task1.php
    Update task1

--- task1.php
+++ task1.php
@@ -1,2 +1,52 @@
-<?
-// Your code is here:
+<?php
+
+
+// Define the folder path name
+
+$folderPath = '/datafiles';
+
+// Define the regex pattern for check file names 
+
+$pattern = '/^[a-zA-Z0-9]+\.ixt$/';
+
+
+// Initialize an array to store the matching file names
+
+$matchingFiles = array();
+
+// Open the directory
+
+if ($handle = opendir($folderPath)) {
+
+    // Iterate file in the directory
+
+    while (false !== ($file = readdir($handle))) {
+
+        // Check the file name matches or not in the pattern
+
+        if (preg_match($pattern, $file)) {
+
+            // Add the matching filename to the array
+
+            $matchingFiles[] = $file;
+        }
+    }
+
+    // Close the directory handle
+
+    closedir($handle);
+}
+
+// Sort the array of matching files
+
+sort($matchingFiles);
+
+// Display the sorted filenames
+
+foreach ($matchingFiles as $file) {
+
+    echo $file . "\n";
+}
+
+?>
+
